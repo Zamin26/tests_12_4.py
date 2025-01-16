d09bbbc6cafd39e2840defeced57ed47aacbf5e8
Zamin26 <zamin26@bk.ru>
2025-01-16 20:54:08 +0300

    Add files via upload

---
+++ tests_12_4.py
@@ -0,0 +1,35 @@
+import logging
+import unittest
+from rt_with_exceptions import Runner
+
+
+class RunnerTest(unittest.TestCase):
+
+    def test_walk(self):
+        try:
+            runner_1 = Runner('Петя', -10)
+            runner_1.walk()
+            self.assertEqual(runner_1.distance, 50)                                   # проверка на условия
+            logging.info(f'"test_walk" выполнен успешно', exc_info=True)                # сообщение при выполнении
+        except ValueError:
+            logging.warning(f'Неверная скорость для Runner', exc_info=True)             # сообщение при ошибке
+
+
+    def test_run(self):
+        try:
+            runner_2 = Runner(50)
+            runner_2.run()
+            self.assertEqual(runner_2.distance, 100)                                  # проверка на условия
+            logging.info(f'"test_run" выполнен успешно', exc_info=True)                 # сообщение при выполнении
+        except TypeError:
+            logging.warning(f"Неверный тип данных для объекта Runner", exc_info=True)   # сообщение при ошибке
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+logging.basicConfig(level=logging.INFO, filemode="w", filename="runner_tests.log", encoding="utf-8",
+                        format="%(asctime)s | %(levelname)s | %(message)s")  # настройка basicConfig
